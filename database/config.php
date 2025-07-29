08d80c2f308a90ed621770b9ebb5274ac88ab8d1
Chris Michael Macalalad Leynes <22-70677@g.batstate-u.edu.ph>
2025-07-29 21:36:56 +0800

    Update config.php

--- database/config.php
+++ database/config.php
@@ -10,5 +10,3 @@ $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "✅ Connected to Aiven using Render environment variables!";
-?>
