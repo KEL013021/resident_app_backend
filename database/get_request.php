78e849678b7c925386a07413f25ef9794ae867ca
Chris Michael Macalalad Leynes <22-70677@g.batstate-u.edu.ph>
2025-07-29 21:06:30 +0800

    Update get_request.php

--- database/get_request.php
+++ database/get_request.php
@@ -1,6 +1,9 @@
 <?php
 include('config.php');
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 try {
     // Decode JSON input
