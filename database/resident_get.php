726b207ed3b142abe787d2e391a5b1337469d92f
Chris Michael Macalalad Leynes <22-70677@g.batstate-u.edu.ph>
2025-07-29 21:05:07 +0800

    Update resident_get.php

--- database/resident_get.php
+++ database/resident_get.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 include('config.php'); // adjust path as needed
 
